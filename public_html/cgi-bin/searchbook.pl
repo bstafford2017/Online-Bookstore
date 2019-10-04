7510fb951727bf41814c1d15252e056fbf3ebcb2
bstafford2017 <b.stafford2017@gmail.com>
2019-10-04 12:10:45 -0500

    Fix conatenation

--- public_html/cgi-bin/searchbook.pl
+++ public_html/cgi-bin/searchbook.pl
@@ -7,6 +7,6 @@ $search =~ s/^\s*(\S*)\s*$/$1/;
 $search =~ s/;|>|>>|<|\*|\?|\&|\|//g;
 
 $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom SearchBook ";
-$cmd .= $search . " ";
+$cmd = join $search, " ";
 
 system($cmd);
