e2c875402ab2dcff2193051700a393991e056147
bstafford2017 <b.stafford2017@gmail.com>
2019-10-09 22:04:47 -0500

    make perl return html

--- public_html/cgi-bin/searchbook.pl
+++ public_html/cgi-bin/searchbook.pl
@@ -2,15 +2,33 @@
 use CGI; 
 $query = new CGI;
 $search = $query->param('search-box');
-print $query->header("text/javascript");
-print $search
+print("Content-type: text/html\n");
+
+print <<EndofHTML;
+  <html>
+   <body>
+    <table width="100%" height="80%">
+     <tr>
+      <td align="center">
+       <font size="+0"><b>
+EndofHTML
 
 $search =~ s/^\s*(\S*)\s*$/$1/;
 $search =~ s/;|>|>>|<|\*|\?|\&|\|//g;
 
 $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom SearchBook ";
 $cmd = join $search, " ";
-
+print($cmd);
 system($cmd);
 
-print($cmd);
+print <<EndofHTML;
+        <form>
+         <input type="button" value=" Back " onclick="history.go(-1);return false;" />
+        </form>
+       </b></font>
+      </td>
+     </tr>
+    </table>
+   </body>
+  </html>
+EndofHTML
