9d21cbd1db3c6011e9968b53a2a44fb046406007
bstafford2017 <b.stafford2017@gmail.com>
2019-10-11 22:08:01 -0500

    strict pl

--- public_html/cgi-bin/searchbook.pl
+++ public_html/cgi-bin/searchbook.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
 use CGI; 
 $query = new CGI;
-$search = $query->param('search-box');
+$search = $query->param('search-box') if defined($search);
 print("Content-type: text/html\n");
 
 print <<EndofHTML;
@@ -13,11 +15,11 @@ print <<EndofHTML;
        <font size="+0"><b>
 EndofHTML
 
-$search =~ s/^\s*(\S*)\s*$/$1/;
-$search =~ s/;|>|>>|<|\*|\?|\&|\|//g;
+$search =~ s/^\s*(\S*)\s*$/$1/ if defined($search);
+$search =~ s/;|>|>>|<|\*|\?|\&|\|//g if defined($search);
 
 $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom SearchBook";
-$cmd = "$cmd $search";
+$cmd = "$cmd $search" if defined($search);
 system($cmd);
 
 print <<EndofHTML;
