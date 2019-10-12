2822a82ebc63febaba790acf4347b5d7da850c30
bstafford2017 <b.stafford2017@gmail.com>
2019-10-12 16:04:58 -0500

    fix search java

--- public_html/cgi-bin/Search.java
+++ public_html/cgi-bin/Search.java
@@ -18,7 +18,7 @@ public class Search {
         try {
             Statement stmt = conn.createStatement();
             String query = "select book.isbn, book.title, book.price, subject.subject_name ";
-            query += "from book inner join subjects on book.isbn = subjects.isbn inner join subject on subject.subject_id = book.subject ";
+            query += "from book inner join subjects on book.isbn = subjects.isbn inner join subject on subject.subject_id = subjects.s_id ";
             if(args.length != 0){
                 query = query + "where book.isbn =" + args[0].trim() + " or book.price = " + args[0].trim() + " or book.title = " +  args[0].trim() + " or subject.subject_name = " + args[0].trim();
             }
--- public_html/cgi-bin/search.pl
+++ public_html/cgi-bin/search.pl
@@ -12,7 +12,7 @@ print <<EndofHTML;
     <table width="100%" height="80%">
 EndofHTML
 
-my $search =~ s/^\s*(\S*)\s*$/$1/ if defined($search);
+$search =~ s/^\s*(\S*)\s*$/$1/ if defined($search);
 $search =~ s/;|>|>>|<|\*|\?|\&|\|//g if defined($search);
 
 my $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom Search";
