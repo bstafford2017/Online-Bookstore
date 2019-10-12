531376a234444090d69e21be9afafb2f380c47ca
bstafford2017 <b.stafford2017@gmail.com>
2019-10-12 17:58:54 -0500

    working subject

--- public_html/cgi-bin/Create.java
+++ public_html/cgi-bin/Create.java
@@ -17,6 +17,11 @@ public class Create {
         Connection conn = ods.getConnection();
         try {
             Statement stmt = conn.createStatement();
+            if(args.length != 4){
+                System.out.println("Content-type: text/html\n");
+                System.out.println("<html><body><p>Invalid number of argments</p></body></html>");
+                Systemn.exit(0);
+            }
             /* Each have a value
                Assumed order is: {isbn, title, price, subject1, subject2, ...} */
             
