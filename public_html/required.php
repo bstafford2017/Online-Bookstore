40b9ef486e8be4485439bda332af20d6ab926f8e
b.stafford2017 <b.stafford2017@gmail.com>
2019-10-27 11:55:56 -0500

    big change to modularity

---
+++ public_html/required.php
@@ -0,0 +1,18 @@
+<!-- Metadata -->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Favicon -->
+<link rel="icon" href="./favicon.png"/>
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
+
+<!-- Background color -->
+<style>
+    html {
+        background-color: rgb(200,200,200);
+    }
+</style>
--- public_html/search_1.php
+++ public_html/search_1.php
@@ -1,15 +1,9 @@
 <!doctype html>
     <head>
-        <meta charset="UTF-8">
-        <link rel="icon" href="./favicon.png"/>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <?php 
+            include 'required.php';
+        ?>
         <title id="title">Search books</title>
-        <link rel="stylesheet" href="style.css">
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
         <script type="text/javascript">
             // Get all subjects on load
             $.ajax({
