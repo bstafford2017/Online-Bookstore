e6cb83a25acdfaed890912b01319dc335b82f1d1
b.stafford2017 <b.stafford2017@gmail.com>
2019-10-27 12:25:44 -0500

    fix more nav

--- public_html/navbar.php
+++ public_html/navbar.php
@@ -1,15 +1,15 @@
 <!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="width: 80%;">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="padding: 5%;">
     <a class="navbar-brand" href="index.html">Bookstore</a>
     <ul class="navbar-nav col-sm-12">
         <li class="nav-item">
-            <a id="home" class="nav-link" href="index.html">Home</a>
+            <a id="home" class="nav-link" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-            <a id="create" class="nav-link" href="create.html">Create</a>
+            <a id="create" class="nav-link" href="create.php">Create</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="search.html">Search Books</a>
+            <a class="nav-link" href="search.php">Search Books</a>
         </li>
     </ul>
 </nav>
