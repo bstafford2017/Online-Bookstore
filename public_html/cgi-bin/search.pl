09613eda65f153cc8ea4be29ea9d6958ebcbb65e
b.stafford2017 <b.stafford2017@gmail.com>
2019-10-26 21:26:04 -0500

    update search

--- public_html/cgi-bin/search.pl
+++ public_html/cgi-bin/search.pl
@@ -9,7 +9,7 @@ $search =~ s/^\s*(\S*)\s*$/$1/ if defined($search);
 $search =~ s/;|>|>>|<|\*|\?|\&|\|//g if defined($search);
 
 print("Content-type: text/html\n");
-system("bin/cat ../search_1.html");
+system("/bin/cat ../search_1.html");
 
 my $compile = "/usr/bin/javac Search.java";
 system($compile);
@@ -18,4 +18,4 @@ my $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom Search";
 $cmd = "$cmd $search" if defined($search);
 system($cmd);
 
-system("bin/cat ../search_2.html");
+system("/bin/cat ../search_2.html");
