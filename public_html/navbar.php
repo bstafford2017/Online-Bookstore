875ac682f0e2201e3c9f43f2d93fe9bd6121bfe7
bstafford2017 <b.stafford2017@gmail.com>
2019-10-27 23:05:18 -0500

    push night

--- public_html/navbar.php
+++ public_html/navbar.php
@@ -1,6 +1,6 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="margin-left: 10%;margin-right: 10%;">
-    <a class="navbar-brand" href="index.html">My Bookstore</a>
+    <a class="navbar-brand" href="index.php">My Bookstore</a>
     <ul class="navbar-nav col-sm-12">
         <li class="nav-item">
             <a id="home" class="nav-link" href="index.php">Home</a>
