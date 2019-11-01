77febbca5b716b673a9e268e0fa0d912fb62ea4d
b.stafford2017 <b.stafford2017@gmail.com>
2019-10-31 20:19:17 -0500

    change menu nav bar

--- navbar.php
+++ navbar.php
@@ -1,6 +1,9 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="margin-left: 10%;margin-right: 10%;">
     <a class="navbar-brand" href="index.php">My Bookstore</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
     <ul class="navbar-nav col-sm-12">
         <li class="nav-item">
             <a id="home" class="nav-link" href="index.php">Home</a>
