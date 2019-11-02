ddb6185c39e3a637e9fba22ea9f76b3a95a7b62f
bstafford2017 <b.stafford2017@gmail.com>
2019-11-02 09:07:38 -0500

    check nul and empty

--- cgi-bin/Create.java
+++ cgi-bin/Create.java
@@ -27,7 +27,7 @@ public class Create {
                 String getSubjectId = "select subject_id from subject where subject_name = " + args[i] + ")";
                 rset = stmt.executeQuery(getSubjectId);
                 String subjectId = rset.getString(1);
-                if(StringUtils.isEmpty(subjectId)) break;
+                if(subjectId == null || subjectId.equals("")) break;
                 // If subject is already in table
                 if(rset.next()){
                     // Only needs to be inserted into subjects (joining table)
