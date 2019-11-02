cdb1be6ddba55112af9de7168e86967dae6e3b0e
bstafford2017 <b.stafford2017@gmail.com>
2019-11-02 14:33:45 -0500

    quick

--- navbar.php
+++ navbar.php
@@ -15,6 +15,9 @@
             <li class="nav-item">
                 <a class="nav-link" href="search.php">Search</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="listbooks.php">List Books</a>
+            </li>
         </ul>
     </div>
 </nav>
