32298f20d06a2ec8afd85870be62748888ab8c40
bstafford2017 <b.stafford2017@gmail.com>
2019-11-02 11:15:35 -0500

    fix hyperlink perl

--- cgi-bin/hyperlink.pl
+++ cgi-bin/hyperlink.pl
@@ -7,9 +7,9 @@ my $isbn = $query->param('isbn');
 
 print("Content-type: text/html\n\n");
 
-my $compile = "/usr/bin/javac Search.java";
+my $compile = "/usr/bin/javac Hyperlink.java";
 system($compile);
 
-my $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom Search ";
+my $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom Hyperlink ";
 $cmd = $cmd . $isbn;
 system($cmd);
