8417b77ddb1b28f78b2b9cbd77298a8ed16e8f81
bstafford2017 <b.stafford2017@gmail.com>
2019-11-02 00:19:59 -0500

    test

--- cgi-bin/create.pl
+++ cgi-bin/create.pl
@@ -4,28 +4,33 @@ use warnings;
 use CGI; 
 my $query = new CGI;
 my $title = $query->param('book-title');
-my $author = $query->param('price');
+my $price = $query->param('price');
 my $isbn = $query->param('isbn');
 my $subjects = $query->param('subjects');
+my @subject = split(' ', $subjects);
+my $allSubjects = "";
+for my $i (@subject){
+    $allSubjects = $allSubjects . " " . $i;
+}
 
-if(!defined $title && !defined $author && !defined $isbn && !defined $subjects){
+if(!defined $title && !defined $price && !defined $isbn && !defined $subjects){
     exit(0);
 }
 
 $title =~ s/^\s*(\S*)\s*$/$1/;
 $title =~ s/;|>|>>|<|\*|\?|\&|\|//g;
-$author =~ s/^\s*(\S*)\s*$/$1/;
-$author =~ s/;|>|>>|<|\*|\?|\&|\|//g;
+$price =~ s/^\s*(\S*)\s*$/$1/;
+$price =~ s/;|>|>>|<|\*|\?|\&|\|//g;
 $isbn =~ s/^\s*(\S*)\s*$/$1/;
 $isbn =~ s/;|>|>>|<|\*|\?|\&|\|//g;
-$subjects =~ s/^\s*(\S*)\s*$/$1/;
-$subjects =~ s/;|>|>>|<|\*|\?|\&|\|//g;
+#$allSubjects =~ s/^\s*(\S*)\s*$/$1/;
+$allSubjects =~ s/;|>|>>|<|\*|\?|\&|\|//g;
 
 my $compile = "/usr/bin/javac Create.java";
 system($compile);
 
 my $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom Create ";
-$cmd = $cmd . " " . $title . " " . $author . " " . $isbn . " " . $subjects;
+$cmd = $cmd . " " . $isbn . " " . $title . " " . $price . " " . $allSubjects;
 print("Content-type: text/html\n\n");
 system($cmd);
 print($cmd);
