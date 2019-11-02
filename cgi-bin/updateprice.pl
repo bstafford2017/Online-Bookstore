e43e36225144d62b6713efb1d9c62e9591849217
bstafford2017 <b.stafford2017@gmail.com>
2019-11-02 17:52:47 -0500

    TEST

--- cgi-bin/updateprice.pl
+++ cgi-bin/updateprice.pl
@@ -3,9 +3,13 @@ use strict;
 use CGI; 
 my $query = new CGI;
 
-my $compile = "/usr/bin/javac Hyperlink.java";
+my $compile = "/usr/bin/javac ListBooks.java";
 system($compile);
 
-my $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom Hyperlink ";
-$cmd = $cmd . $isbn;
+my $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom ListBooks ";
 system($cmd);
+
+my @names = $query->param;
+foreach my $name (@names) {
+    $cmd = $cmd . " " . $name . " " . query->param($name);
+}
