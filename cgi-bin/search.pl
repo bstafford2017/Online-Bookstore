2fdc367dbd90f5db9c39776323fd3a3884e8878c
bstafford2017 <b.stafford2017@gmail.com>
2019-11-02 10:26:48 -0500

    change

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -5,14 +5,18 @@ use CGI;
 my $query = new CGI;
 my $search = $query->param('search-box');
 
-$search =~ s/^\s*(\S*)\s*$/$1/ if defined($search);
-$search =~ s/;|>|>>|<|\*|\?|\&|\|//g if defined($search);
+if(!defined $search){
+    exit(0);
+}
+
+$search =~ s/^\s*(\S*)\s*$/$1/;
+$search =~ s/;|>|>>|<|\*|\?|\&|\|//g;
 
 print("Content-type: text/html\n\n");
 
 my $compile = "/usr/bin/javac Search.java";
 system($compile);
 
-my $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom Search";
-$cmd = "$cmd $search" if defined($search);
+my $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom Search ";
+$cmd = $cmd . $search;
 system($cmd);
