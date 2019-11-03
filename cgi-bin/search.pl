079e63cf8b847e45a00228e897e9a407a89f695b
b.stafford2017 <b.stafford2017@gmail.com>
2019-11-03 13:43:19 -0600

    test

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -6,12 +6,16 @@ my $query = new CGI;
 my $search = $query->param('search');
 my $subject = $query->param('subject');
 
-# Fix for subjects with spaces
-$subject =~ s/-/ /g;
-
 if(!defined $search){
     exit(0);
 }
+
+if(!defined $subject){
+    exit(0);
+}
+
+# Fix for subjects with spaces
+$subject =~ s/-/ /g;
 
 $search =~ s/^\s*(\S*)\s*$/$1/;
 $search =~ s/;|>|>>|<|\*|\?|\&|\|//g;
