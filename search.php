044ffbcd9e342f55e4452ee8ef2daefccb218d76
b.stafford2017 <b.stafford2017@gmail.com>
2019-11-03 13:13:14 -0600

    yay think i got it

--- search.php
+++ search.php
@@ -8,7 +8,6 @@
             $.ajax({
                 type: "get",
                 url: "cgi-bin/subject.cgi",
-                async: false,
                 success: function(data){
                     $(function(){
                         $('#subject').append(data);
