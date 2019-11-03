3682b73e08faf1dc54ca969a7221c141df4fc4f8
b.stafford2017 <b.stafford2017@gmail.com>
2019-11-03 14:19:27 -0600

    test

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -6,23 +6,18 @@ my $query = new CGI;
 my $search = $query->param('search');
 my $subject = $query->param('subject');
 
-if(!defined $search){
-    exit(0);
-}
-
-if(!defined $subject){
-    exit(0);
-}
-
-$search =~ s/^\s*(\S*)\s*$/$1/;
-$search =~ s/;|>|>>|<|\*|\?|\&|\|//g;
-
 print("Content-type: text/html\n\n");
-
 my $compile = "/usr/bin/javac Search.java";
 system($compile);
-
-my $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom Search ";
-$cmd = $cmd . $search . " " . $subject;
+my $cmd = "";
+if(!defined $search && !defined $subject){
+    $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom Search ";
+} elsif(!defined $search){
+    $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom Search " . $subject;
+} elsif(!defined $subject){
+    $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom Search " . $search;
+} else {
+    $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom Search ". $search . " " . $subject;
+}
 system($cmd);
 print($cmd);
