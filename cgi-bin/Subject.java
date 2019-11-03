a2ca9ff165e70bbd1287cf07f85d15686aa7ec75
b.stafford2017 <b.stafford2017@gmail.com>
2019-11-03 13:16:50 -0600

    final fix??

--- cgi-bin/Subject.java
+++ cgi-bin/Subject.java
@@ -22,7 +22,7 @@ public class Subject {
             int rowCounter = 0;
             while(rset.next()){
                 System.out.println("<div class=\"form-check form-check-inline\">");
-                System.out.println("<input class=\"form-check-input\" type=\"checkbox\" name=\"subject\" id=\"defaultCheck" + rowCounter + "\" value=\"" + rset.getString(1) + "\">");
+                System.out.println("<input class=\"form-check-input\" type=\"checkbox\" name=\"subject\" id=\"defaultCheck" + rowCounter + "\" value=\"" + rset.getString(1) + "\"/>");
                 rowCounter++;
             }
             if(rowCounter == 0){
