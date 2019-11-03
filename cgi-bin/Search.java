7378ae0c9905022979b2fb9e78d25d29ffeb58ef
b.stafford2017 <b.stafford2017@gmail.com>
2019-11-03 15:26:05 -0600

    big test for search AND

--- cgi-bin/Search.java
+++ cgi-bin/Search.java
@@ -33,7 +33,38 @@ public class Search {
         Connection conn = ods.getConnection();
         Statement stmt = conn.createStatement();
         try {
-            int counter = 0;
+            String query = "select book.isbn, book.title, book.price, subject.subject_name ";
+            query += "from book join subjects on book.isbn = subjects.isbn join subject on subject.subject_id = subjects.s_id where ";
+            for(int i = 0; i < arg.length; i++){
+                query = query + "(book.title like '%" + args[counter].trim().replace("-", " ") + "%' or subject.subject_name like '%" + args[counter].trim().replace("-", " ") + "%' or book.isbn like '%" +  args[counter].trim().replace("-", " ") + "%' or book.price like '%" + args[counter].trim().replace("-", " ") + "%')";
+                // Always add 'and' to end if not the last iteration
+                if(i < arg.length - 1){
+                    query += " and ";
+                }
+            }
+            ResultSet rset = stmt.executeQuery(query);
+            LinkedList<Tuple> list = new LinkedList<>();
+            while(rset.next()){
+                if(!Search.containsOrAdd(list, rset.getString(4), Long.parseLong(rset.getString(1)))){
+                    list.add(new Tuple(Long.parseLong(rset.getString(1)), rset.getString(2), Double.parseDouble(rset.getString(3)), rset.getString(4)));
+                }
+            }
+            if(list.size() == 0){
+                System.out.println("<p>No results!</p>");
+            }
+            Iterator<Tuple> it = list.iterator();
+            int rowCounter = 0;
+            while(it.hasNext()){
+                Tuple current = it.next();
+                System.out.println("<tr id=\"" + rowCounter + "\" scope=\"col\">");
+                System.out.println("<td id=\"isbn\">" + current.isbn + "</td>");
+                System.out.println("<td id=\"title\" scope=\"col\"><a href=\"cgi-bin/hyperlink.cgi?isbn=" + current.isbn + "\">" + current.title + "</a></td>");
+                System.out.println("<td id=\"price\" scope=\"col\">" + current.price + "</td>");
+                System.out.println("<td id=\"subjects\" scope=\"col\">" + current.subjects + "</td></tr>");
+                rowCounter++;
+            }
+
+            /*int counter = 0;
             do {
                 String query = "select book.isbn, book.title, book.price, subject.subject_name ";
                 query += "from book join subjects on book.isbn = subjects.isbn join subject on subject.subject_id = subjects.s_id ";
@@ -63,7 +94,7 @@ public class Search {
                 }
                 counter++;
                 rset.close();
-            } while(counter < args.length);
+            } while(counter < args.length);*/
             stmt.close();
         }
         catch (SQLException ex) {
@@ -86,7 +117,7 @@ public class Search {
         }
         
         // If in list, return avoid inserting into list
-        if(flag) return false;
+        if(!flag) return false;
 
         // If in in list, add 'subject' into isbn's record
         Iterator<Tuple> newit = list.iterator();
