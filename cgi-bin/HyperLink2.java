2b874a6cb6aae80e25056435c924297f9fcd725f
b.stafford2017 <b.stafford2017@gmail.com>
2019-11-03 22:41:08 -0600

    push

--- cgi-bin/HyperLink2.java
+++ cgi-bin/HyperLink2.java
@@ -16,7 +16,7 @@ public class Hyperlink2 {
         try {
             Statement stmt = conn.createStatement();
             String query = "select book.title, book.isbn, subject.subject_name from book join subjects on subjects.isbn = book.isbn join subject on subject.subject_id = subjects.s_id where subject.subject_name like '%" + args[0].trim().replace("-", " ") + "%'";
-            System.out.println(query);
+            System.out.println("<p>" + query + "</p>");
             ResultSet rset = stmt.executeQuery(query);
             int counter = 0;
             while(rset.next()){
