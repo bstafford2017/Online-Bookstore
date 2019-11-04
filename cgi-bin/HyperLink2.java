c23ca91a930f978269aa57f63acc821f91e0ea89
b.stafford2017 <b.stafford2017@gmail.com>
2019-11-03 21:26:58 -0600

    test

--- cgi-bin/HyperLink2.java
+++ cgi-bin/HyperLink2.java
@@ -15,12 +15,12 @@ public class Hyperlink {
         Connection conn = ods.getConnection();
         try {
             Statement stmt = conn.createStatement();
-            String query = "select book.isbn, book.title, book.price, subject.subject_name from book join subjects on subjects.isbn = book.isbn join subject on subject.subject_id = subjects.s_id where book.isbn = " + args[0].trim();
+            String query = "select book.title, subject.subject_name from book join subjects on subjects.isbn = book.isbn join subject on subject.subject_id = subjects.s_id where subject.subject_name = " + args[0].trim();
             ResultSet rset = stmt.executeQuery(query);
             int counter = 0;
             while(rset.next()){
                 if(counter == 0) {
-                    System.out.println("ISBN: "+ rset.getString(1) + "\nTitle: " + rset.getString(2) + "\nPrice: " + rset.getString(3) + "\nSubjects: " + rset.getString(4));
+                    System.out.println("Title: "+ rset.getString(1) + "\nSubjects: " + rset.getString(2));
                 } else {
                     System.out.println(rset.getString(4));
                 }
--- cgi-bin/hyperlink.pl
+++ cgi-bin/hyperlink.pl
@@ -4,16 +4,28 @@ use warnings;
 use CGI; 
 my $query = new CGI;
 my $isbn = $query->param('isbn');
-
-if(! defined $isbn){
-    exit(0);
-}
+my $subjects = $query->param('subjects');
 
 print("Content-type: text/html\n\n");
 
-my $compile = "/usr/bin/javac Hyperlink.java";
-system($compile);
+if(! defined $isbn){
+    exit(0);
+} else {
+    my $compile = "/usr/bin/javac Hyperlink.java";
+    system($compile);
 
-my $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom Hyperlink ";
-$cmd = $cmd . $isbn;
+    my $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom Hyperlink ";
+    $cmd = $cmd . $isbn;
+}
+
+if(! defined $subjects){
+    exit(0);
+} else {
+    my $compile = "/usr/bin/javac Hyperlink2.java";
+    system($compile);
+
+    my $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom Hyperlink2 ";
+    $cmd = $cmd . $subjects;
+}
+
 system($cmd);
