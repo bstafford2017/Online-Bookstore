7eb5116940fcaf2c8fa50e625ca6124611fae7b2
bstafford2017 <b.stafford2017@gmail.com>
2019-11-05 17:07:48 -0600

    test

--- cgi-bin/delete.pl
+++ cgi-bin/delete.pl
@@ -3,10 +3,13 @@ use strict;
 use warnings;
 use CGI; 
 my $query = new CGI;
-my $isbn = query->param('isbn');
 
 my $compile = "/usr/bin/javac Delete.java";
 system($compile);
 
 my $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom Delete " . $isbn;
+my @names = $query->param;
+foreach my $name (@names) {
+    $cmd = $cmd . " " . $name . " " . $query->param($name);
+}
 system($cmd);
