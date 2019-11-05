61c1416fc8df750422a95a73693424565ca024a0
bstafford2017 <b.stafford2017@gmail.com>
2019-11-04 18:30:25 -0600

    fix delete?

--- cgi-bin/Delete.java
+++ cgi-bin/Delete.java
@@ -19,38 +19,20 @@ public class Delete {
             // Get list of all subject's in isbn record
             Statement stmt = conn.createStatement();
             for(int i = 0; i < args.length; i++){
-                String query = "delete from (select * from book join subjects on book.isbn = subjects.isbn join subject on subject.subject_id = subjects.s_id) ";
-                query = query + "where book.isbn = " + args[i].trim();
-                stmt.executeUpdate(query);
-            }
-            /*stmt.executeUpdate("delete from book where isbn = " + args[0].trim());
-            String query = "select subject.subject_name ";
-            query += "from book join subjects on book.isbn = subjects.isbn join subject on subject.subject_id = subjects.s_id ";
-            query = query + "where isbn = " + args[0].trim();
-            ResultSet rset = stmt.executeQuery(query);
-            LinkedList<String> allSubjects = new LinkedList<>();
-            while(rset.next()){
-                allSubjects.add(rset.getString(1));
-            }
-
-            // Loop through book's subjects to see if need to delete or not
-            ListIterator<String> it = allSubjects.listIterator();
-            while(it.hasNext()){
-                String current = it.next();
-                String newQuery = "count(select subject.subject_name ";
-                newQuery += "from book join subjects on book.isbn = subjects.isbn join subject on subject.subject_id = subjects.s_id ";
-                newQuery = newQuery + "where isbn = " + args[0].trim() + ")";
-                ResultSet set = stmt.executeQuery(newQuery);
-                
-                // Delete if count of that subject is equal to 0
-                if(Integer.parseInt(set.getString(1)) == 0){
-                    String getId = "select subject.subject_id from subject where subject.subject_name = " + current;
-                    ResultSet idSet = stmt.executeQuery(getId);
-                    int id = Integer.parseInt(idSet.getString(1));
-                    stmt.executeUpdate("delete from subjects where s_id = " + id);
-                    stmt.executeUpdate("delete from subject where subject_name = " + current);
+                stmt.executeUpdate("delete from subject where exists (select * subject.subject_name from subjects join book on book.isbn = subjects.isbn join subject on subject.subject_id = subject.subject_id where book.isbn = " + args[i].trim() + ")");
+                ResultSet set = stmt.executeQuery("select subject.subject_name from subjects join book on book.isbn = subjects.isbn join subject on subject.subject_id = subject.subject_id where book.isbn = " + args[i].trim());
+                while(set.next()){
+                    String currentSubject = set.getString(1);
+                    ResultSet getCount = stmt.executeQuery("select count(*) from subjects join book on book.isbn = subjects.isbn join subject on subject.subject_id = subject.subject_id where subject.subject_name like'%" + currentSubject + "%'");
+                    int count = 0;
+                    if(getCount.next()){
+                        count = Integer.parseInt(getCount.getString(1));
+                    }
+                    if(count == 0){
+                        stmt.executeUpdate("delete from subjects where subject_name like '%" + currentSubject + "%'");
+                    }
                 }
-            }*/
+            }
             stmt.close();
         }
         catch (SQLException ex) {
