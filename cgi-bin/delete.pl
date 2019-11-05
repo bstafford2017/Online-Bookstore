72f830e11e895cbb09c08e4ee0b759bfbc85443b
bstafford2017 <b.stafford2017@gmail.com>
2019-11-05 17:08:19 -0600

    test

--- cgi-bin/delete.pl
+++ cgi-bin/delete.pl
@@ -7,7 +7,7 @@ my $query = new CGI;
 my $compile = "/usr/bin/javac Delete.java";
 system($compile);
 
-my $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom Delete " . $isbn;
+my $cmd = "/usr/bin/java -Djava.security.egd=file:/dev/./urandom Delete ";
 my @names = $query->param;
 foreach my $name (@names) {
     $cmd = $cmd . " " . $name . " " . $query->param($name);
