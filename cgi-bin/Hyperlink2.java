26e3cb52289ff196040a48446c1fc7924a77d995
bstafford2017 <b.stafford2017@gmail.com>
2019-11-05 23:13:29 -0600

    Finally FIX HYPERLINK!

--- cgi-bin/Hyperlink2.java
+++ cgi-bin/Hyperlink2.java
@@ -20,12 +20,13 @@ public class Hyperlink2 {
             int counter = 0;
             while(rset.next()){
                 if(counter == 0) {
-                    System.out.println("<p>Title: <a href=\"hyperlink.cgi?isbn="+ rset.getString(2) + "\">" + rset.getString(1) + "</a></p><p>Subjects: " + rset.getString(3) + "</p>");
+                    System.out.println("<p>ISBN: "+ rset.getString(1) + "</p><p>Title: " + rset.getString(2) + "</p><p>Price: " + rset.getString(3) + "</p><p>Subjects: <a href=\"hyperlink.cgi?subjects=" + rset.getString(4).replace(" ", "-") + "\">" + rset.getString(4) + "</a> ");
                 } else {
-                    System.out.println(rset.getString(2));
+                    System.out.println("<a href=\"hyperlink.cgi?subjects=" + rset.getString(4).replace(" ", "-") + "\"" + rset.getString(4) + "</a>");
                 }
                 counter++;
             }
+            System.out.println("</p>");
             rset.close();
             stmt.close();
         }
