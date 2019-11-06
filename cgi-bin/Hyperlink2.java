07fb103ec6ac2b4056589300577b594e072115e4
bstafford2017 <b.stafford2017@gmail.com>
2019-11-05 23:03:45 -0600

    test

--- cgi-bin/Hyperlink2.java
+++ cgi-bin/Hyperlink2.java
@@ -20,12 +20,13 @@ public class Hyperlink2 {
             int counter = 0;
             while(rset.next()){
                 if(counter == 0) {
-                    System.out.println("<p>Title: <a href=\"hyperlink.cgi?isbn="+ rset.getString(2) + "\">" + rset.getString(1) + "</a></p><p>Subjects: " + rset.getString(3) + "</p>");
+                    System.out.println("<p>Subject: " + rset.getString(3) + "</p><p>All titles: <a href=\"hyperlink.cgi?isbn="+ rset.getString(2) + "\">" + rset.getString(1) + "</a> ");
                 } else {
-                    System.out.println(rset.getString(2));
+                    System.out.println("<a href=\"hyperlink.cgi?isbn="+ rset.getString(2) + "\">" + rset.getString(1) + "</a>");
                 }
                 counter++;
             }
+            System.out.println("</p>");
             rset.close();
             stmt.close();
         }
