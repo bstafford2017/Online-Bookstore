abd9664ee02317f491a2373935dba55b651617dc
b.stafford2017 <b.stafford2017@gmail.com>
2019-11-06 11:30:46 -0600

    add all!

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+    <head>
+        <?php 
+            include 'required.php';
+        ?>
+        <title id="title">Home</title>
+    </head>
+    <body>
+        <?php
+            include 'navbar.php';
+        ?>
+        <h2 style="text-align:center;">Welcome to the bookstore!<h2>
+        <p><a id="source" href="#">View Source</a></p>
+        <div id="display-source"></div>
+        <?php
+            include 'footer.php';
+        ?>
+        <script>
+            $('#source').click(function(e){
+                e.preventDefault();
+                $.ajax({
+                    method: "GET",
+                    url: "cgi-bin/source.cgi",
+                    data: {"filename": "search"},
+                    success: function(data){
+                        $('html').empty();    
+                        $('html').html(data);
+                    },
+                    error: function(data){
+                        $('html').empty();    
+                        $('html').html(data);
+                    }
+                });
+            });
+        </script>
+    </body>
+</html>
