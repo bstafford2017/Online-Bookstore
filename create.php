33b705ba4db26ae40fe6d6bb5b8d5ec568a8ce96
b.stafford2017 <b.stafford2017@gmail.com>
2019-11-08 13:09:41 -0600

    test

--- create.php
+++ create.php
@@ -58,11 +58,11 @@
                     data: {filename: "search"},
                     success: function(data){
                         $('body').empty();    
-                        $('body').append(data);
+                        $('body').append("<pre>" + data + "</pre");
                     },
                     error: function(data){
                         $('body').empty();    
-                        $('body').append(data);
+                        $('body').append("<pre>" + data + "</pre");
                     }
                 });
             });
