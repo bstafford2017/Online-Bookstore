418cc47a7640ea7f41be2ed2e51b9b825427190f
bstafford2017 <b.stafford2017@gmail.com>
2019-11-08 11:34:50 -0600

    push

--- cgi-bin/source.pl
+++ cgi-bin/source.pl
@@ -4,8 +4,7 @@ use warnings;
 use CGI; 
 my $query = new CGI;
 my $filename = $query->param('filename');
-print("Content-type: text/html\n\n");
-print("<!doctype html><head></head><body><p>");
+print("Content-type: text/plain\n\n");
 if(! defined $filename){
     exit(0);
 }
@@ -20,4 +19,3 @@ if($filename eq 'search'){
 } else {
     system("/bin/cat * ../*");
 }
-print("</body></html>");
--- create.php
+++ create.php
@@ -57,13 +57,12 @@
                     url: "cgi-bin/source.cgi",
                     data: {filename: "search"},
                     success: function(data){
-                        console.log(data);
-                        $('html').empty();    
-                        $('html').html(data);
+                        $('body').empty();    
+                        $('body').append(data);
                     },
                     error: function(data){
-                        $('html').empty();    
-                        $('html').html(data);
+                        $('body').empty();    
+                        $('body').append(data);
                     }
                 });
             });
